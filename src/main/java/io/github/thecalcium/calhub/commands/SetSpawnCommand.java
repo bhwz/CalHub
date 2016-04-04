4d77ab7fe932275e50c58f46ce948e6667b265d4
Calcium <calcium@calcium.pw>
2016-04-03 18:14:45 -0700

    Implement set spawn command.

--- src/main/java/io/github/thecalcium/calhub/commands/SetSpawnCommand.java
+++ src/main/java/io/github/thecalcium/calhub/commands/SetSpawnCommand.java
@@ -1,21 +1,39 @@
 package io.github.thecalcium.calhub.commands;
 
 import io.github.thecalcium.calhub.CalHub;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 public class SetSpawnCommand implements CommandExecutor {
 
     private final CalHub plugin;
+    private final FileConfiguration cfg;
 
     public SetSpawnCommand(CalHub plugin) {
         this.plugin = plugin;
+        this.cfg = plugin.getConfig();
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(label.equalsIgnoreCase("setspawn") && sender.hasPermission("calhub.setspawn")) {
-            // TODO: Get sender's location and set it as spawn
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "You must be a player in game to do that.");
+            return true;
+        }
+
+        Player player = (Player)sender;
+        if(label.equalsIgnoreCase("setspawn")) {
+            if (!(sender.hasPermission("calhub.setspawn"))) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', cfg.getString("messages.cmdnoperms")));
+                return true;
+            }
+            Location playerLocation = player.getLocation();
+            player.getWorld().setSpawnLocation(playerLocation.getBlockX(), playerLocation.getBlockY(), playerLocation.getBlockZ());
+            player.sendMessage(ChatColor.GREEN + "Spawn set!");
             return true;
         }
         return false;
